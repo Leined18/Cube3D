72ef28ce229903efa80d0259e9b4e896e5b51a11
DaNieL <142941184+Leined18@users.noreply.github.com>
2025-03-10 12:13:52 +0100

    Initial commit

---
+++ inc/methods.h
@@ -0,0 +1,10 @@
+#ifndef MTMETHODS_H
+# define MTMETHODS_H
+
+# include "tetris.h"
+
+// ==================== Memory Management ====================
+
+
+
+#endif
---
+++ inc/tetris.h
@@ -0,0 +1,13 @@
+
+#ifndef MT_H
+# define MT_H
+
+# include "structs.h"
+# include "methods.h"
+# include "utils.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+# define VISITED ((void *)-1)
+
+#endif // MT_H
